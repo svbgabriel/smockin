bb5cc32666814e36db4fe76d849c07a811e9a3a3
Gabriel Batista <svbgabriel@proton.me>
2026-01-24 10:56:04 -0300

    Remove all install related scripts, configurations, and resources from the repository

--- debug_with_inmem.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-APP_VERSION="2.20.2"
-
-mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Dapp.version=$APP_VERSION -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8008"
